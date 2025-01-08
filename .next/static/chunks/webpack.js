8ca5ba3de975a928e3bf80ae71158a828fbff91e
Stephen Warren <jayarex07@gmail.com>
2025-01-08 15:41:44 -0600

    Added Company Fonts to SCSS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4346ee06efdbac06"; }
+/******/ 		__webpack_require__.h = function() { return "eb9cdb79df4ad277"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
