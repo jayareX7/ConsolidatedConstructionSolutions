48d72ccc4a1b7912458000557dc7b48666fdb91e
Stephen Warren <jayarex07@gmail.com>
2025-01-14 16:39:34 -0600

    added Icon component for TinaCMS template

---
+++ src/components/Icons.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const Icon = () => <FontAwesomeIcon icon={faCheckCircle} />;
+
+export default Icon;
