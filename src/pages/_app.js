93c3b19d5709849c93ba2f284f63a5303788eb2f
Stephen Warren <jayarex07@gmail.com>
2025-02-25 18:43:17 -0600

    Set up Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,14 +18,14 @@ function MyApp({ Component, pageProps }) {
       dataLayer.push(arguments);
     }
     gtag('js', new Date());
-    gtag('config', appData.googleanalytics.measurementID);
+    gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
   }, []);
 
   return (
     <>
 
 <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${appData.googleanalytics.measurementID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '{${appData.googleanalytics.measurementID}}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
 
