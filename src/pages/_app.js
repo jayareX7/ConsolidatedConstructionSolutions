32ac21ce6ef7c860a6681ca12b74cb179b25b198
Stephen Warren <jayarex07@gmail.com>
2025-02-25 18:38:10 -0600

    Set up Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Head from "next/head";
 import appData from "@content/settings/setting.json";
-
+import Script from "next/script";
+import { useEffect } from "react";
 import '../styles/scss/style.scss';
 import "../styles/globals.css";
 
@@ -10,11 +11,32 @@ import { register } from "swiper/element/bundle";
 register();
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', appData.googleanalytics.measurementID);
+  }, []);
+
   return (
     <>
 
+<Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${appData.googleanalytics.measurementID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '{${appData.googleanalytics.measurementID}}');
+        `}
+      </Script>
 
-    
       <Head>
           {/* seo begin */}
           <title>{appData.siteSettings.siteName}</title>
