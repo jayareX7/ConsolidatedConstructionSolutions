7f18001335569a91b347c5d32c6a98e453161106
Stephen Warren <jayarex07@gmail.com>
2025-03-05 01:39:48 -0600

    updated media root

--- tina/config.ts
+++ tina/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: "pages",
-      publicFolder: "public",
+      publicFolder: "pages",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
